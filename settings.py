496c19a1177c1ca59b0637e0fc8f7f82a75c1f25
iwootten <i.wootten@gmail.com>
2016-07-04 15:48:56 +0100

    Revert "Use custom logging handler to create log directory"
    This reverts commit 91561c4c6f5f740b7d16a86c39a7be9f254cc3fa.

--- settings.py
+++ settings.py
@@ -1,30 +1,7 @@
 import logging
-import logging.handlers
-import os
-import errno
 
 logger = logging.getLogger(__name__)
 
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-validate: %(message)s"
 LOGGING_LOCATION = "logs/validate.log"
 LOGGING_LEVEL = logging.DEBUG
-
-
-def mkdir_p(path):
-    """http://stackoverflow.com/a/600612/190597 (tzot)"""
-    try:
-        os.makedirs(path, exist_ok=True)
-    except TypeError:
-        try:
-            os.makedirs(path)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else:
-                raise
-
-
-class MakeFileHandler(logging.handlers.RotatingFileHandler):
-    def __init__(self, filename, **kwargs):
-        mkdir_p(os.path.dirname(filename))
-        logging.handlers.RotatingFileHandler.__init__(self, filename, **kwargs)
