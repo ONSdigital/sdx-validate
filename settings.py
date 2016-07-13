c4367e35987ab4a71cfcca5e33f5949767414f90
Ian Wootten <i.wootten@gmail.com>
2016-07-13 11:34:57 +0100

    Remove log handler to file

--- settings.py
+++ settings.py
@@ -3,5 +3,4 @@ import logging
 logger = logging.getLogger(__name__)
 
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-validate: %(message)s"
-LOGGING_LOCATION = "logs/validate.log"
 LOGGING_LEVEL = logging.DEBUG
