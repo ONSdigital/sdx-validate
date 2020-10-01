6d6365d2c3cfa32699a17bdb6dd6c4403b268e60
Jon Kent <jonkent95@gmail.com>
2020-10-01 15:57:58 +0100

    remove dev from startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,4 @@ then
     export PORT=5000; 
 fi
 
-if [ "$SDX_DEV_MODE" = true ]
-then
-    python3 server.py
-else
-    gunicorn -b 0.0.0.0:$PORT server:app
-fi
+gunicorn -b 0.0.0.0:$PORT server:app
